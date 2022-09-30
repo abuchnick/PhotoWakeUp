773d5697bd6b07cfb7e78920923885cb39ff5f4e
Asaf Buchnick <asaf.buchnick@aiola.com>
2022-09-30 18:12:48 +0300

    fixed background inpainting
    for now not used

--- src/background_inpainting.py
+++ src/background_inpainting.py
@@ -1,15 +1,8 @@
 import cv2
 
-img_path = r'./data/images_temp/man.jpg'
-mask_path = r'./data/images_temp/man_refine_mask.jpeg'
-out_path = r'./data/images_temp/man_inpainted.png'
 
-img = cv2.imread(filename=img_path)
-mask = cv2.imread(filename=mask_path, flags=cv2.IMREAD_GRAYSCALE)
-
-result = cv2.inpaint(src=img, inpaintMask=mask, inpaintRadius=30, flags=cv2.INPAINT_TELEA)
-
-cv2.imwrite(filename=out_path, img=result)
-# cv2.imshow('inpainted', result)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+def inpaint_background(img_path, segmentation_mask_path, out_path):
+    img = cv2.imread(filename=img_path)
+    mask = cv2.imread(filename=segmentation_mask_path, flags=cv2.IMREAD_GRAYSCALE)
+    result = cv2.inpaint(src=img, inpaintMask=mask, inpaintRadius=30, flags=cv2.INPAINT_TELEA)
+    cv2.imwrite(filename=out_path, img=result)
